edd28e880dff572ded620302714c8936c1abc916
wnso <wnso.kim@gmail.com>
2023-05-08 15:43:22 +0900

    first commit

---
+++ src/main/java/com/perfume/surfing/SurfingApplication.java
@@ -0,0 +1,13 @@
+package com.perfume.surfing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SurfingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SurfingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/perfume/surfing/SurfingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.perfume.surfing;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SurfingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
